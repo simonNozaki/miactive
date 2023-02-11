4fc7a1abddc4a139a060bf67fde15cb718dec686
snozaki <snozaki@netprotections.co.jp>
2023-02-11 22:31:50 +0900

    refactor: ProxyHandlerのreceiverに型をつける

--- src/reactive.ts
+++ src/reactive.ts
@@ -8,11 +8,12 @@ import { track, trigger } from "./effect"
 export const reactive = <T extends object>(obj: T): T => {
   const handler: ProxyHandler<any> = {
     // target[key]とReflect.get(target, key)は外側から見ると等価
-    get: (target: T, key: string | symbol, receiver) => {
+    get: (target: T, key: string | symbol, receiver: typeof Proxy) => {
       track(target, key)
+      // receiverを設定することで参照するthisをreciver(のproxy)に束縛する
       return Reflect.get(target, key, receiver)
     },
-    set: (target: T, key: string | symbol, newValue: unknown, receiver): boolean => {
+    set: (target: T, key: string | symbol, newValue: unknown, receiver: typeof Proxy): boolean => {
       Reflect.set(target, key, newValue)
       trigger(target, key)
       return Reflect.set(target, key, newValue, receiver)
