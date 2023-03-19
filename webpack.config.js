b5830910d5abe322dccc71647cdd3ed170dc69d4
snozaki <snozaki@netprotections.co.jp>
2023-03-19 21:14:24 +0900

    feat ビルド成果物に複数のhtml, jsを含める

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProduction = process.env.NODE_ENV == 'production';
 const stylesHandler = MiniCssExtractPlugin.loader;
 
+// 複数のentryを受け付ける: https://shuu1104.com/2021/12/4458/
+const entries = WebpackWatchedGlobEntries.getEntries([path.resolve(__dirname, './src/*.ts')], {
+  ignore: path.resolve(__dirname, './src/_*.ts'),
+})();
+
 const config = {
-  entry: './src/index.ts',
+  // entry: './src/index.ts',
+  entry: entries,
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
@@ -17,6 +24,10 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'stock.html',
+      filename: 'stock.html'
     }),
     new MiniCssExtractPlugin({
       filename: './public/css/style.css'
