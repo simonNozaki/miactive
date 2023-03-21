f5f236349730b90e8228c26986b8012c52518298
snozaki <snozaki@netprotections.co.jp>
2023-03-21 15:56:07 +0900

    テスト足りてない

--- spec/effect.spec.ts
+++ spec/effect.spec.ts
@@ -2,12 +2,20 @@ import { reactive } from '../src/reactive'
 import { effect } from '../src/effect'
 
 describe('effect spec', () => {
-  it('should track deps', () => {
-    const product = reactive({
+  interface Product {
+    price: number
+    quantity: number
+  }
+
+  it('should track deps in reactive', () => {
+    const product = reactive<Product>({
       price: 5,
       quantity: 2
     })
     let total = 0
     effect(() => (total = product.price * product.quantity))
+    expect(total).toBe(10)
+    product.price = 7
+    expect(total).toBe(14)
   })
 })
